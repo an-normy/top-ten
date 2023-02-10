6255b365423264fa278fce427b1c57737be22b38
an-normy <112652463+an-normy@users.noreply.github.com>
2023-02-10 13:04:50 -0300

    iaokdowafrfwdfa

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ function getMenu(){
 function addItem(){
     var htmldata;
     var item=document.getElementById("addItem").value;
-    var imgtags = '<img id="im1" src="images/pizzaImg.png"/>'
+    var imgtags = '<img id="im1" src="pizzaImg.png"/>'
     menuListArray.sort();
     htmldata = ""
-    for (var i = 0;i<menuListArray;i++) {
+    for (var i = 0;i<menuListArray.length;i++) {
         htmldata = htmldata + menuListArray[i] + '<br>';
     }
     document.getElementById("displayAddedMenu").innerHTML = htmldata;
